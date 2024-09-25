b0188be958605f1a6dd0470eebbe66897a73dd86
ramiz181 <ramizmalik78685@gmail.com>
2024-09-25 08:55:20 -0700

    Update app.js
    adding one more pattern...

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+// pattern 01
+
 for (let i = 1; i < 13; i += 2) {
 
 
@@ -30,5 +33,24 @@ for (let i = 13; i >= 1; i -= 2) {
 
     }
     document.write("<br />")
+
+}
+
+// pattern 02
+
+for (let i = 3; i <= 13; i += 2) {
+
+    for (let j = 13; j > i; j--) {
+
+        document.write("&nbsp");
+
+    }
+
+    for (let k = 0; k < i; k++) {
+
+        document.write("*");
+
+    }
+    document.write("<br />");
 
 }
